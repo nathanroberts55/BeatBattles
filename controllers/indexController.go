9464bdf5e4464663a7a98db77f31f86378d44e5c
Dylan Hackworth <me@dylhack.dev>
2024-05-12 19:32:07 -0500

    remove year and title params

--- controllers/indexController.go
+++ controllers/indexController.go
@@ -1,23 +1,14 @@
 package controllers
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/gofiber/fiber/v2"
-)
+import "github.com/gofiber/fiber/v2"
 
 type indexProps struct {
 	Params
-	Year  string
-	Title string
 }
 
 func HomeIndex(c *fiber.Ctx) error {
 	props := indexProps{
 		defaultParams,
-		fmt.Sprint(time.Now().Year()),
-		"Beat Battle",
 	}
 	return c.Render("index", props)
 }

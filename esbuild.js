4747c5351f40cbd53e523d78f1460b94ae4ba05a
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-05-14 22:04:04 -0400

    Some cooking for the night

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,21 @@
 import esbuild from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
 
-const context = await esbuild.context({
+await esbuild.build({
 	entryPoints: ['public/scss/custom.scss'],
 	outdir: 'public/css',
 	bundle: true,
 	minify: true,
 	plugins: [sassPlugin()],
 });
+
+// const context = await esbuild.context({
+// 	entryPoints: ['public/scss/custom.scss'],
+// 	outdir: 'public/css',
+// 	bundle: true,
+// 	minify: true,
+// 	plugins: [sassPlugin()],
+// });
 
 // Enable watch mode
 // await context.watch();

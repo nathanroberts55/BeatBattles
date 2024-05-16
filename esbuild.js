d549cd4992f80cdd24b79eb55be476399a024600
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-05-15 21:11:45 -0400

    Happy Now?

--- esbuild.js
+++ esbuild.js
@@ -1,21 +1,12 @@
 import esbuild from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
 
-await esbuild.build({
-	entryPoints: ['public/scss/custom.scss'],
-	outdir: 'public/css',
-	bundle: true,
-	minify: true,
-	plugins: [sassPlugin()],
-});
-
-// const context = await esbuild.context({
-// 	entryPoints: ['public/scss/custom.scss'],
-// 	outdir: 'public/css',
-// 	bundle: true,
-// 	minify: true,
-// 	plugins: [sassPlugin()],
-// });
-
-// Enable watch mode
-// await context.watch();
+if (process.env['NODE_ENV'] !== 'production') {
+	await esbuild.build({
+		entryPoints: ['public/scss/custom.scss'],
+		outdir: 'public/css',
+		bundle: true,
+		minify: true,
+		plugins: [sassPlugin()],
+	});
+}

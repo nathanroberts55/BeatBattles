beabec5a41d662a1028fea11258c71db33cbfa6f
Nate R <45110045+nathanroberts55@users.noreply.github.com>
2024-06-21 22:07:44 -0400

    Nate/UI changes (#12)

--- server/server.go
+++ server/server.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/html/v2"
 )
 
@@ -10,6 +13,14 @@ func New() *fiber.App {
 		Views:       html.New("./views", ".html"),
 		ViewsLayout: "layouts/base",
 	})
+
+	// Logging remote IP and Port
+	app.Use(logger.New(logger.Config{
+		Format:     "${time} | [${ip}:${port}] ${status} - ${method} ${path}\n",
+		TimeFormat: "02-Jan-2006 15:04:05",
+		TimeZone:   "UTC",
+		Output:     os.Stdout,
+	}))
 
 	return app
 }

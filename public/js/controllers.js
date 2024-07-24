ff84d358093368a15ed22adcb4a207f85c8c1d4f
Dylan Hackworth <me@dylhack.dev>
2024-07-24 10:24:35 -0500

    Disable Join when Streamer field is empty

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,9 @@
 import { Application } from "https://unpkg.com/@hotwired/stimulus@3.2.2/dist/stimulus.js"
 import SongController from "./controllers/song.js"
+import FormController from "./controllers/form.js"
 
 window.Stimulus = Application.start()
 window.Stimulus.debug = window.location.protocol === "http:"
 window.Stimulus.register("song", SongController)
-
+window.Stimulus.register("form", FormController)
 
